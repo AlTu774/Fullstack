faab550f4bf73ba88718e4dbb73a8c78625f364f
AlTu774 <alisa.t1@outlook.com>
2024-01-09 00:15:10 +0200

    unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,23 +5,48 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [total, setTotal] = useState(0)
+  
+  const Good = () => {
+      setGood(good + 1)
+      setTotal(total + 1)
+    }
+  const Neutral = () => {
+      setNeutral(neutral + 1)
+      setTotal(total + 1)
+    }
+  const Bad = () => {
+      setBad(bad + 1)
+      setTotal(total + 1)
+    }
+
+  const Button = (props) => {
+      return (
+      <button onClick={props.onClick}>
+        {props.feedback}
+      </button>
+      )
+    }
 
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick ={() => setGood(good + 1)}>
-        good
-      </button>
-      <button onClick ={() => setNeutral(neutral + 1)}>
-        neutral
-      </button>
-      <button onClick ={() => setBad(bad + 1)}>
-        bad
-      </button>
+      <Button
+      onClick = {Good}
+      feedback="good"/>
+      <Button
+      onClick = {Neutral}
+      feedback="neutral"/>
+      <Button
+      onClick = {Bad}
+      feedback="bad"/>
       <h1>statistics</h1>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {total}</p>
+      <p>average {(good-bad)/total}</p>
+      <p>positive {good/total}%</p>
     </div>
   )
 }
