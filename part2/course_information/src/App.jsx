b5795ed3f991c0729f2f3cacd9d591c158291ad8
AlTu774 <alisa.t1@outlook.com>
2024-01-27 21:54:02 +0200

    course info step6

--- part2/course_information/src/App.jsx
+++ part2/course_information/src/App.jsx
@@ -30,25 +30,8 @@ const Total = (props) => {
   )
 }
 
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-
+const Course = (props) => {
+  const course = props.course
   return (
     <div>
       <Header course={course} />
@@ -56,6 +39,32 @@ const App = () => {
       <Total course={course} />
     </div>
   )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
