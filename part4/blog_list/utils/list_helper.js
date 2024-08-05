166656eec326ecf1ea6db66e1e06d29a22ba3e35
AlTu774 <alisa.t1@outlook.com>
2024-08-04 22:02:54 -0400

    helper function unit tests step2

--- part4/blog_list/utils/list_helper.js
+++ part4/blog_list/utils/list_helper.js
@@ -1,7 +1,18 @@
 const dummy = (blogs) => {
     return 1
   }
-  
+
+const totalLikes = (blogs) => {
+    if (blogs == [] || null) {
+        return 0
+    }
+    const likeSum = blogs.reduce((likes, blog) => {
+        return (likes+blog.likes)
+    },0)
+    return (likeSum)
+}
+
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }
