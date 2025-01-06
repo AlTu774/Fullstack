45807cdc37e484e2bc4c6c81ec3b393081d21296
AlTu774 <alisa.t1@outlook.com>
2025-01-07 00:50:38 +0200

    blog list tests step 2

---
+++ part4/blog_list/tests/blog_test_helper.js
@@ -0,0 +1,24 @@
+
+const initialBlogs = [
+    {
+        title: "About Something",
+        author: "User1",
+        url: "blogs.com/article1",
+        likes: 5
+    },
+    {
+        title: "New Testing",
+        author: "User2",
+        url: "blogs.com/article2",
+        likes: 2
+    },
+    {
+        title: "Something Else",
+        author: "User1",
+        url: "blogs.com/article3",
+        likes: 1
+    }
+
+]
+
+module.exports = { initialBlogs }
--- part4/blog_list/utils/config.js
+++ part4/blog_list/utils/config.js
@@ -3,7 +3,6 @@ dotenv.config()
 
 const PORT = process.env.PORT
 
-//const MONGODB_URL1 = process.env.MONGODB_URL
 
 const MONGODB_URL = process.env.NODE_ENV === 'test' 
   ? process.env.TEST_MONGODB_URL
