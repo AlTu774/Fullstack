62c31246469fd8a8d925f1869aae3aed4928d134
AlTu774 <alisa.t1@outlook.com>
2025-01-07 23:38:25 +0200

    blog list test step 3

--- part4/blog_list/tests/blog_api.test.js
+++ part4/blog_list/tests/blog_api.test.js
@@ -27,7 +27,7 @@ test("there are 3 notes initially", async () => {
     const response =  await api.get("/api/blogs")
     .expect('Content-Type', /application\/json/)
 
-    assert.strictEqual(response.body.length, 3)  
+    assert.strictEqual(response.body.length, helper.initialBlogs.length)  
 })
 
 test("notes contain unique identifier id", async () => {
@@ -38,6 +38,45 @@ test("notes contain unique identifier id", async () => {
         assert.notDeepEqual(blog.id, undefined)
     })
 })
+
+test("adding a blog to the database works", async () => {
+    const blog = {
+        title: "New Blog",
+        author: "User3",
+        url: "blogs.com/article2",
+        likes: 2
+    }
+    
+    await api
+    .post("/api/blogs")
+    .send(blog)
+
+    const response = await api.get("/api/blogs")
+    
+    assert.strictEqual(response.body.length, helper.initialBlogs.length+1)  
+})
+
+test("the right blog gets added to the database", async () => {
+    const blog = {
+        title: "New Blog",
+        author: "User3",
+        url: "blogs.com/article2",
+        likes: 2
+    }
+    
+    await api
+    .post("/api/blogs")
+    .send(blog)
+
+    const response = await api.get("/api/blogs")
+    const newestBlog = response.body[response.body.length-1]
+
+    assert.equal(newestBlog.title, blog.title)
+    assert.equal(newestBlog.author, blog.author)
+    assert.equal(newestBlog.url, blog.url)
+    assert.equal(newestBlog.likes, blog.likes)
+})
+
 
 after(async () => {
     await mongoose.connection.close()
