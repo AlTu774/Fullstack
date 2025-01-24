1d0aeadddd258c269c3f61a56ffe563e91003501
AlTu774 <alisa.t1@outlook.com>
2025-01-24 20:20:28 +0200

    blog list expansion step 10

--- part4/blog_list/controllers/blogs.js
+++ part4/blog_list/controllers/blogs.js
@@ -1,7 +1,6 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const User = require('../models/user')
-const jwt = require('jsonwebtoken')
+const middleware = require('../utils/middleware')
 
 
 blogsRouter.get('/',  async (request, response) => {
@@ -11,15 +10,9 @@ blogsRouter.get('/',  async (request, response) => {
   })
   
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', middleware.userExtractor,async (request, response) => {
   const content = request.body
-
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({ error: 'token invalid' })
-  }
-  const user = await User.findById(decodedToken.id)
-  content.user = user.id
+  const user = request.user
 
   if (content.likes == undefined) {
     content.likes = 0
@@ -29,7 +22,7 @@ blogsRouter.post('/', async (request, response) => {
     response.status(400).end()
 
   } else {
-    
+    content.user = user.id
     const blog = new Blog(content)
     const result = await blog.save()
     
@@ -42,16 +35,28 @@ blogsRouter.post('/', async (request, response) => {
   }
   })
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', middleware.userExtractor, async (request, response) => {
   const deleteId = request.params.id
-  await Blog.findByIdAndDelete(deleteId)
-  
-  response.status(204).end()
+  const user = request.user
+  const blog = await Blog.findById(deleteId)
+  if (blog == undefined) {
+    response.status(204).end()
+  }
+
+  if (blog.user == user.id) {
+    await Blog.findByIdAndDelete(deleteId)
+    response.status(204).end()
+  }
+  else {
+    return response.status(401).json({ error: 'unauthorized to delete blog' })
+  }
+
 })
 
 blogsRouter.put('/:id', async (request, response) => {
   const changeId = request.params.id
   const updatedBlog = await Blog.findByIdAndUpdate(changeId, request.body, {new: true})
+  .populate("user", { username: 1, name: 1 })
 
   response.json(updatedBlog)
 })
--- part4/blog_list/utils/middleware.js
+++ part4/blog_list/utils/middleware.js
@@ -1,3 +1,6 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user')
+
 
 const tokenExtractor = (request, response, next) => {
     const authorization = request.get('authorization')
@@ -6,6 +9,17 @@ const tokenExtractor = (request, response, next) => {
     }
     next()
   }
+
+const userExtractor = async (request, response, next) => {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    } 
+
+    const user = await User.findById(decodedToken.id)
+    request.user = user
+    next()
+}
   
 
 const errorHandler = (error, request, response, next) => {
@@ -29,5 +43,6 @@ const unknownEndpoint = (request, response) => {
 module.exports = { 
     errorHandler,
     unknownEndpoint,
-    tokenExtractor
+    tokenExtractor,
+    userExtractor
 }
