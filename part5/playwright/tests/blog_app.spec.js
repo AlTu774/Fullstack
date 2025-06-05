dd1388e11af9fe13836412d375aba36c432437c6
AlTu774 <alisa.t1@outlook.com>
2025-06-05 19:46:07 +0300

    blog list e2e step 3

--- part5/playwright/tests/blog_app.spec.js
+++ part5/playwright/tests/blog_app.spec.js
@@ -1,4 +1,5 @@
 const { test, expect, beforeEach, describe } = require('@playwright/test')
+import loginWith from './helper'
 
 describe('Blog app', () => {
   beforeEach(async ({ page, request }) => {
@@ -20,20 +21,39 @@ describe('Blog app', () => {
 
   describe('Login', () => {
     test('succeeds with correct credentials', async ({ page }) => {
-      await page.getByTestId('username').fill('user123')
-      await page.getByTestId('password').fill('pass123')
-      await page.getByRole('button', {name:'login'}).click()
+      //await page.getByTestId('username').fill('user123')
+      //await page.getByTestId('password').fill('pass123')
+      //await page.getByRole('button', {name:'login'}).click()
+      await loginWith(page, 'user123', 'pass123')
 
       await expect(page.getByText('user123 has logged in')).toBeVisible()
     })
 
     test('fails with wrong credentials', async ({ page }) => {
-      await page.getByTestId('username').fill('user3')
+      /*await page.getByTestId('username').fill('user3')
       await page.getByTestId('password').fill('pass')
       await page.getByRole('button', {name:'login'}).click()
+      */
+      await loginWith(page, 'user3', 'pass')
 
       await expect(page.getByText('wrong username or password')).toBeVisible()
       await expect(page.getByText('user123 has logged in')).not.toBeVisible()
+    })
+
+    describe('When logged in', () => {
+      beforeEach(async ({ page }) => {
+        await loginWith(page, 'user123', 'pass123')
+      
+      })
+      test('a new blog can be created', async ({ page }) => {
+        await page.getByRole('button', {name: 'create'}).click()
+        await page.getByPlaceholder('title').fill('New Blog')
+        await page.getByPlaceholder('author').fill('Some author')
+        await page.getByPlaceholder('url').fill('site.com')
+        await page.getByRole('button', {name: 'create'}).click()
+
+        await expect(page.getByText('New Blog Some author')).toBeVisible()
+      })
     })
   })
 })
