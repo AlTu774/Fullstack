6834e827f6dbd54bce38a90a179de71bac4388a6
AlTu774 <alisa.t1@outlook.com>
2025-06-24 14:22:38 +0300

    anecdotes step 5

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,42 +1,17 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { createAnecdote, voteAnecdote } from './reducers/anecdoteReducer'
+import voteAnecdote from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotesStore = useSelector(state => state.sort((a,b) => b.votes-a.votes))
   let anecdotes = anecdotesStore.sort((a, b) => a.value - b.value)
   const dispatch = useDispatch()
 
-/*
-  const vote = (id) => {
-    dispatch({
-      type: 'VOTE',
-      payload: {
-        id: id
-      }
-    })
-  }
-
-  const createHandler = (event) => {
-    event.preventDefault()
-    dispatch({
-      type: 'NEW_ANECDOTE',
-      payload: {
-        content: event.target.content.value
-      }
-    })
-  }
-*/
-
   const vote = (id) => {
     dispatch(voteAnecdote(id))
   }
-
   
-  const createHandler = (event) => {
-    event.preventDefault()
-    dispatch(createAnecdote(event.target.content.value))
-  }
-
+/*
   return (
     <div>
       <h2>Anecdotes</h2>
@@ -58,6 +33,26 @@ const App = () => {
       </form>
     </div>
   )
+*/
+
+return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <AnecdoteForm/>
+    </div>
+  )
+
 }
 
 export default App
---
+++ part6/redux-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,23 @@
+import useDispatch from 'react-redux'
+import createAnecdote from './reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+  const dispatch = useDispatch()
+  
+  const createHandler = (event) => {
+    event.preventDefault()
+    dispatch(createAnecdote(event.target.content.value))
+  }
+
+  return(
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={createHandler}>
+        <div><input name='content'/></div>
+        <button type='submit'>create</button>
+      </form>
+    </div>
+  )
+}
+
+export default AnecdoteForm
