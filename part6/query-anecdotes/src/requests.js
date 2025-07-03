fd2bac46026c9dcca2dfdb80b889ef7a94b13cfd
AlTu774 <alisa.t1@outlook.com>
2025-07-03 04:36:21 +0300

    exercise 6.21

--- part6/query-anecdotes/src/requests.js
+++ part6/query-anecdotes/src/requests.js
@@ -1,10 +1,15 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/anecdotes'
+const baseUrl = 'http://localhost:3001/anecdotes/'
 
 export const getAll = async() => {
   const response = await axios.get(baseUrl)
   return response.data
 }
 
-export default { getAll }
+export const createAnecdote = async(anecdote) => {
+  const response = await axios.post(baseUrl, anecdote)
+  return response.data
+}
+
+export default { getAll, createAnecdote }
